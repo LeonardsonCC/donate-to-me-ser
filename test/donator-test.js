0478c67336f8ece4d27570e8ccb342c5014210c3
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-09-30 20:17:14 -0300

    passing tests :)

---
+++ test/donator-test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Donator", function () {
+  it("Should donate some money to me", async function () {
+    const Donator = await ethers.getContractFactory("Donator");
+    const donate = await Donator.deploy();
+    await donate.deployed();
+
+    const makeDonation = await donate.donate({ value: ethers.utils.parseEther("5") });
+
+    // wait until the transaction is mined
+    await makeDonation.wait();
+
+    expect(await donate.getCountDonations()).to.equal(1);
+    expect(await donate.getTotalDonations()).to.equal(ethers.utils.parseEther("5"));
+  });
+});
